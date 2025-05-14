c1d9899c804e9afd42d9aa3e53cfc7ae5c02bbd8
GorbunovDanil <danil.gorbunov@tul.cz>
2025-05-14 08:33:54 +0200

    Define backend project structure

---
+++ backend/burza_project/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from api.views import StartViewSet, RatingViewSet
+from django.urls import include, path
+
+router = DefaultRouter()
+router.register(r"start",  StartViewSet, basename="start")
+router.register(r"rating", RatingViewSet, basename="rating")
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
